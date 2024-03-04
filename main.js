74daa69a67bf7cb6befe6ced2e4aabd696c886ec
GrabovyiS <graboviys@yandex.ru>
2024-03-04 16:25:24 +0300

    feat: player's rock/paper/scissors choice

---
+++ main.js
@@ -0,0 +1,19 @@
+// Get user input
+function getPlayerSelection() {
+  return prompt('Choose your fighter').toLowerCase();
+}
+
+// Randomize and get computer's turn
+function getComputerSelection() {
+
+}
+
+// Play one round
+function playRound(playerSelection, computerSelection) {
+  
+}
+
+// Play a five round match of rock paper scissors and keeps score
+function playGame() {
+
+}
